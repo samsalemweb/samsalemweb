15bc4ff67ef6bc869840e2af28af4f7769aeeb60
samsalemweb <arealtor158@gmail.com>
2026-06-21 23:53:48 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import AnimationProvider from "@/components/animations/AnimationProvider";
-import { GoogleTagManager } from "@next/third-parties/google";
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.sam-salem.com"),
@@ -26,8 +27,25 @@ export default function RootLayout({
           <main className="min-h-screen">{children}</main>
         </AnimationProvider>
         <Footer />
-        <GoogleTagManager gtmId="GTM-TCP6QF2H" />
+       
       </body>
+      <Script
+    id="gtm-head"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];
+        w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TCP6QF2H');
+      `,
+    }}
+  />
     </html>
   );
 }
